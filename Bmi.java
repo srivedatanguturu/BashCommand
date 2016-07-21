b3783f3984bce2b2072a69c6c705e2c888cbffd0
srivedatanguturu <tanguturusriveda@gmail.com>
2016-07-21 19:32:40 -0400

    Update Bmi.java

--- Bmi.java
+++ Bmi.java
@@ -1,3 +1,6 @@
-#!/bin/bash
-# My first script
-$echo "this should be displayed as the output on the console output window when i run the bash echo command."
+public class HelloWorld {
+    public static void main(String[] args) { 
+        System.out.println("Hello, World");
+        $echo "this should be displayed as the output on the console output window when i run the bash echo command."
+    }
+}
