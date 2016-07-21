09bc2b98a28cdbc474f54b481f59632522b48ba8
srivedatanguturu <tanguturusriveda@gmail.com>
2016-07-21 19:34:34 -0400

    Update Bmi.java

--- Bmi.java
+++ Bmi.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) { 
         System.out.println("Hello, World");
-        $echo "this should be displayed as the output on the console output window when i run the bash echo command."
     }
 }
